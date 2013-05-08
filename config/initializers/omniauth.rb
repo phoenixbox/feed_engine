87b05529c837275059518b049fc8b57bf99576ab
Aimee Maher <aimee@geekycheeks.com>
2013-05-08 15:24:10 -0600

    Finished authentication through Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, 'DkOkGk45mZXju35soiMmw', 'vkUeufA5lEmWH7pfZGyjg0uPrbPCu93rIKl54pT1bs'
 end
