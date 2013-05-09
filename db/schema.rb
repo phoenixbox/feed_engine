7134c2ad09230015e0a0bd4573f084ba554db31d
Aimee Maher <aimee@geekycheeks.com>
2013-05-09 11:42:12 -0600

    fixed capitalized twitter handle issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508210303) do
+ActiveRecord::Schema.define(:version => 20130509173254) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130508210303) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "subdomain"
   end
 
 end
