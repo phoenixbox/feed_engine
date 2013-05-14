10167356b201533ab896098d5289d38075130a20
Josh Mejia <joshua.mejia@gmail.com>
2013-05-13 22:05:44 -0600

    added ability to submit post to a user's feed using hstore

--- app/controllers/feed_items_controller.rb
+++ app/controllers/feed_items_controller.rb
@@ -1,7 +1,11 @@
 class FeedItemsController < ApplicationController
 
   def create
-    fail
-    feed_item = FeedItem.new(params["feed_item"])
+    @feed_item = FeedItem.create_from_tuneline(params[:feed_item], current_user)
+    if @feed_item.save
+      redirect_to :back, notice: "Successfully added item to feed"
+    else
+      redirect_to :back, notice: "Sorry pal"
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_subdomain!(request.subdomain)
+    @feeds = @user.feed_items
     @feed_item = FeedItem.new
   end
 
