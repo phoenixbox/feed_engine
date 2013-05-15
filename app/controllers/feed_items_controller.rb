743095516261b0e1d0371b696eba4fe1928f9a9e
Shane Rogers <rogerssh@tcd.ie>
2013-05-15 14:01:03 -0600

    reversed order content

--- app/controllers/feed_items_controller.rb
+++ app/controllers/feed_items_controller.rb
@@ -1,7 +1,6 @@
 class FeedItemsController < ApplicationController
 
   def create
-    binding.pry
     @feed_item = FeedItem.create_from_tuneline(params[:feed_item], current_user)
     if @feed_item.save
       redirect_to :back, notice: "Successfully added item to feed"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_subdomain!(request.subdomain)
-    @feeds = current_user.feed_items
+    @feeds = current_user.feed_items.order('created_at DESC')
     @feed_item = FeedItem.new
   end
 
