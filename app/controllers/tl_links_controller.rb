9d1948c104b200e8033d7b24ea0a91feaf01e368
Josh Mejia <joshua.mejia@gmail.com>
2013-05-17 09:11:13 -0600

    removed binding.pry from tl_link

--- app/controllers/tl_links_controller.rb
+++ app/controllers/tl_links_controller.rb
@@ -1,9 +1,7 @@
 class TlLinksController < ApplicationController
 
   def create
-    binding.pry
     @tl_link = current_user.tl_links.create_from_form(params[:tl_link])
-    # @tl_link = current_user.tl_links.create(params[:feed_item])
     if @tl_link.save
       redirect_to :back, notice: "Successfully added item to feed"
     else
