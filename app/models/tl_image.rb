96ec4f709ac59bc9329c0c4cab5ab9d389da55e9
Aimee Maher <aimee@geekycheeks.com>
2013-05-18 13:36:14 -0600

    pictures work with ImageMagick

--- app/models/tl_image.rb
+++ app/models/tl_image.rb
@@ -1,15 +1,14 @@
 class TlImage < FeedItem
   validates_length_of :content, :maximum => 256
-  validates_presence_of :image
+  validates_presence_of :photo
 
   def self.create_from_form(input)
     create! do |tl_image|
       tl_image.data["content"] = input["content"]
-      tl_image.data["image"] = input["image"]
     end
   end
 
   def valid_attrs
-    %w[content image]
+    %w[content]
   end
 end
