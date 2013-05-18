eb91c6aeb50300ddd2c3386d2af184f83c289cd9
Josh Mejia <joshua.mejia@gmail.com>
2013-05-18 12:41:07 -0600

    added lastfm omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    fail
     #auth = env['omniauth.auth']
     user = User.from_omniauth(env["omniauth.auth"])
     #if user.persisted?
