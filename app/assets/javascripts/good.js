e5fe798ee6d87978e87fac139f25f39e1fc7d448
Christopher <ChrisKnight.mail@gmail.com>
2013-05-20 16:28:21 -0600

    new div sizes and setup for ajax

--- app/assets/javascripts/good.js
+++ app/assets/javascripts/good.js
@@ -1,9 +1,6 @@
 function playSomeSound(genre) {
   SC.get('/tracks', {
-    genres: genre,
-    bpm: {
-      from: 100
-    }
+    genres: genre
   }, function(tracks){
     var random = Math.floor(Math.random() * 49);
     SC.oEmbed(tracks[random].uri, { auto_play: true }, document.getElementById('target'));
--- app/controllers/tl_images_controller.rb
+++ app/controllers/tl_images_controller.rb
@@ -2,10 +2,9 @@ class TlImagesController < ApplicationController
 
   def create
     @tl_image = current_user.tl_images.create(params[:tl_image])
-    if @tl_image.save
-      redirect_to :back, notice: "Successfully added item to feed"
-    else
-      redirect_to :back, notice: "Sorry pal"
+    respond_to do |format|
+      format.html { redirect_to :back  }
+      format.js
     end
   end
 end
