1ea127ab916c7ee5d6ba257124d5f14f47992db6
Shane Rogers <rogerssh@tcd.ie>
2013-05-20 14:30:59 -0600

    Redis Initializer announces connection info

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,8 @@
+puts %{
+================================================================
+  REDIS Connecting at #{ENV["REDISTOGO_URL"]}
+================================================================
+}
+
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
