558b8847a77615d7e459816c0c4cf27246d1d875
Christopher <ChrisKnight.mail@gmail.com>
2013-05-20 12:01:35 -0600

    soundcloud player plays genres

---
+++ app/assets/javascripts/good.js
@@ -0,0 +1,26 @@
+function playSomeSound(genre) {
+  SC.get('/tracks', {
+    genres: genre,
+    bpm: {
+      from: 100
+    }
+  }, function(tracks){
+    var random = Math.floor(Math.random() * 49);
+    SC.oEmbed(tracks[random].uri, { auto_play: true }, document.getElementById('target'));
+  });
+}
+
+window.onload = function() {
+  SC.initialize({
+    client_id: 'ffb7dc6e9ae3f482155566e06f917c29'
+  });
+
+  var menuLinks = document.getElementsByClassName('genre');
+  for (var i = 0; i < menuLinks.length; i++){
+    var menuLink = menuLinks[i];
+    menuLink.onclick = function(e) {
+      e.preventDefault();
+      playSomeSound(menuLink.innerHtml);
+    };
+  }
+};
