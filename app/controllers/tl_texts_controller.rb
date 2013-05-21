48a37bc646939958d2845d06577f7748868bb3f1
Christopher <ChrisKnight.mail@gmail.com>
2013-05-21 11:39:20 -0600

    working on ajax

--- app/controllers/tl_texts_controller.rb
+++ app/controllers/tl_texts_controller.rb
@@ -2,10 +2,9 @@ class TlTextsController < ApplicationController
 
   def create
     @tl_text = current_user.tl_texts.create_from_form(params[:tl_text])
-    if @tl_text.save
-      redirect_to :back, notice: "Successfully added item to feed"
-    else
-      redirect_to :back, notice: "Your message must be between 1-512 characters"
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
     end
   end
 end
