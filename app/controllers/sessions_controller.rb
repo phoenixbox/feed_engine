ce454cde402f54c3f277fef72c1858e77e1a1f36
Josh Mejia <joshua.mejia@gmail.com>
2013-05-22 19:11:32 -0600

    rerouted user to their page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url(subdomain: current_user.subdomain)
+    redirect_to myaccount_path
   end
 
   def destroy
