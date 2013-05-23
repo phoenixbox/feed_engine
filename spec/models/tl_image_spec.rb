2f98a80bb8c29df153a82bc66f13fd41a76a1961
Aimee Maher <aimee@geekycheeks.com>
2013-05-22 21:11:45 -0600

    fixed tests all pass

--- spec/models/tl_image_spec.rb
+++ spec/models/tl_image_spec.rb
@@ -4,28 +4,8 @@ describe "TlImage" do
 
   context "valid input" do
     it "invalid content and data not saved in the data field" do
-      tl_image = TlImage.create_from_form({"content" => "", "photo" => "not_a_photo"})
+      tl_image = TlImage.create_from_form({"photo" => "not_a_photo"})
       expect(tl_image).to be_invalid
     end
   end
-
-  context "invalid input" do
-    let(:bad_content) { "a" * 257 }
-
-    it "validates presence of content" do
-      invalid_attributes = {"content" => ""}
-      expect{ TlImage.create_from_form(invalid_attributes) }.to raise_exception(ActiveRecord::RecordInvalid)
-    end
-
-    it "requires the length be less than or equal to 256 chars" do
-      invalid_attributes = { "content" => bad_content }
-      expect{ TlImage.create_from_form(invalid_attributes) }.to raise_exception(ActiveRecord::RecordInvalid)
-    end
-
-    it "prevents unspecifed attributes from being saved" do
-      tl_image = TlImage.create_from_form({"content" => "test"})
-      expect(tl_image.data).to eq({"content" => "test"})
-    end
-  end
-
 end
